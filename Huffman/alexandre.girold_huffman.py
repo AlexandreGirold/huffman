1fd330483ff234cada16ac24a9dea91caf307c17
alexandre.girold <alexandre.girold@epita.fr>
2022-12-16 14:40:18 +0100

    Should Work ?

--- Huffman/alexandre.girold_huffman.py
+++ Huffman/alexandre.girold_huffman.py
@@ -34,7 +34,7 @@ def build_frequency_list(dataIN):
             freq.append((1, dataIN[i]))
     return freq
 
-# L = build_frequency_list("bbaabtttaabtctce")
+L = build_frequency_list("um ah human huffman is fun i am a fan ha ha ha ha ha ha")
 
 def __sort_by_freq(tupl_list):
     """
@@ -47,8 +47,6 @@ def __sort_by_freq(tupl_list):
             j -= 1
     return tupl_list
         
-
-#print(__sort_by_freq(build_frequency_list("dddfgdfgf")))
 
 def build_Huffman_tree(inputList):
     """
@@ -64,7 +62,7 @@ def build_Huffman_tree(inputList):
         H.push((c1 + c2, ht))
     return H.pop()[1]    
 
-# HT = build_Huffman_tree(L)
+HT = build_Huffman_tree(L)
 
 def __prettyprint(HT):
     """
@@ -100,7 +98,6 @@ def encode_data(huffmanTree, dataIN):
         final += __encode_data(huffmanTree, "", elem)
     return final
     
-# encode_data(HT, 'bbaabtttaabtctce')
 
 def __ascii_to_binary (data):
     """
@@ -201,35 +198,80 @@ def compress(dataIn): #Not the same result as the example
 ################################################################################
 ## DECOMPRESSION
 
+def __decode_data(huffmanTree, DataIn, Output, index):
+
+    if huffmanTree == None:
+        return Output
+    else:
+        if DataIn[index] == "0":
+            return __decode_data(huffmanTree.left, )
+
 def decode_data(huffmanTree, dataIN):
     """
     Decode a string using the corresponding huffman tree into something more readable.
     """
-    # FIXME
-    pass
-    
+    output = ""
+    tree = huffmanTree
+
+    for el in dataIN:
+        if el == "0":
+            tree = tree.left
+        else:
+            tree = tree.right
+        if tree.left == None and tree.right == None:
+            output += tree.key
+            tree = huffmanTree
+    return output
+
+enc = encode_data(HT, 'um ah human huffman is fun i am a fan ha ha ha ha ha ha')
+print(enc)
+print(decode_data(HT, enc))
+
+def __decode_tree(DataIN, len, index):
+    chrt = ""
+    if DataIN[index[0]] == "1":
+        for i in range(8):
+            chrt += DataIN[index[0] + i + 1]
+        chrt = __binary_to_asccii(chrt)
+        tree = bintree.BinTree(chrt, None, None)
+        index[0] += 9
+    else:
+        index[0] += 1
+        tree = bintree.BinTree(None, __decode_tree(DataIN, index, len), __decode_tree(DataIN, index, len))
+    return tree
+
 def decode_tree(dataIN):
     """
     Decodes a huffman tree from its binary representation:
         * a '0' means we add a new internal node and go to its left node
         * a '1' means the next 8 values are the encoded character of the current leaf         
     """
-    # FIXME
-    pass
-    
+    index = [0]
+    n = len(dataIN)
+    return __decode_tree(dataIN, index, n)
 
 
 def from_binary(dataIN, align):
     """
     Retrieve a string containing binary code from its real binary value (inverse of :func:`toBinary`).
     """
-    # FIXME
-    pass
+    l = len(dataIN)
+    data = ""
+    for i in range(l):
+        if i == l - 1:  # ignore the align first zeros
+            temp = __ascii_to_binary(ord(dataIN[i]))
+            for j in range(align, 8):
+                data += temp[j]
+        else:
+            data += __ascii_to_binary(ord(dataIN[i]))
+
+    return data
 
 
 def decompress(data, dataAlign, tree, treeAlign):
     """
     The whole decompression process.
     """
-    # FIXME
-    pass
+    data = from_binary(data, dataAlign)
+    tree = from_binary(tree, treeAlign)
+    return decode_data(decode_tree(tree), data)
